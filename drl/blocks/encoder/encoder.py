757e6a8a3766cfa03c99ed5ebea83efd9116e6b6
lokhiufung <lokhiufung123@gmail.com>
2020-11-18 21:24:42 +0800

    added core

---
+++ drl/blocks/encoder/encoder.py
@@ -0,0 +1,27 @@
+from torch import nn
+
+from drl.core.modules import TrainableModule
+
+
+class Encoder(TrainableModule):
+    def __init__(self, n_layers, hidden_dim, input_dim, output_dim):
+        super(Encoder, self).__init__()
+        self.n_layers = n_layers
+        self.hidden_dim = hidden_dim
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        layers = []
+        for i in self.n_layers - 1:
+            if i == 0:
+                layers.append(nn.Linear(self.input_dim, self.hidden_dim))
+            elif i == self.n_layers - 1:
+                layers.append(nn.Linear(self.hidden_dim, self.hidden_dim))
+            layers.append(nn.ReLU)
+        layers.append(nn.Linear(self.hidden_dim, self.output_dim))
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
---
+++ drl/core/modules.py
@@ -0,0 +1,59 @@
+from torch import nn
+
+
+class Module:
+    """
+    Basic unit of agent
+    """
+        
+class NonTranableModule(Module):
+    """
+    Module with no trainable weights
+    """
+    _is_trainable = False
+
+    @property
+    def is_trainable(self):
+        return self._is_trainable
+
+
+class TrainableModule(Module):
+    """
+    Module with trainable weights
+    """
+    _is_trainable = True
+
+    @property
+    def is_trainable(self):
+        return self._is_trainable
+
+    @property
+    def weights(self):
+        return self._get_weights()
+    
+    def _get_weights(self):
+        raise NotImplementedError
+
+    @property
+    def is_freeze(self):
+        """
+        whether 
+        """
+        raise NotImplementedError
+
+    def freeze(self):
+        """
+        freeze all trainable weights
+        """
+        raise NotImplementedError
+
+
+class TorchTrainableModule(TrainableModule, nn.Module):
+    """
+    This Module are parametrized and trained with pytorch
+    """
+    def forward(self):
+        raise NotImplementedError
+
+    def _get_weigths(self):
+        return self.model_parameters()
